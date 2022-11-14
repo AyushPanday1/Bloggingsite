71a67b9a0b34b4743049bd97b45461d24c3cc060
AyushPanday1 <ayush7426883261@gmail.com>
2022-11-14 15:59:08 +0530

    Half

--- src/model/Authormodel.js
+++ src/model/Authormodel.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
 
+var validateEmail = function(email) {
+   var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   return re.test(email)
+};
+
 const Authormodel = new mongoose.Schema({
   
      fname : {
@@ -18,8 +23,14 @@ const Authormodel = new mongoose.Schema({
      },
 
      email : {
-        required :true,
-        unique:true,
+       unique:true,
+       type: String,
+       trim: true,
+       lowercase: true,
+       unique: true,
+       required: 'Email address is required',
+       validate: [validateEmail, 'Please fill a valid email address'],
+       match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
      },
 
      password: {
