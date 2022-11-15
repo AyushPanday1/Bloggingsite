4fde682fa6ba0bcde5115434d118321066135e3e
vishnu25 <vsdubey025@gmail.com>
2022-11-15 09:20:08 -0800

    edge case

--- src/controller/putcontroller.js
+++ src/controller/putcontroller.js
@@ -1,11 +1,20 @@
 const blogmodel = require('../model/blogmodel')
 const mongoose = require('mongoose');
+const{isValidObjectId} = require("mongoose")
 
 
 
 
 const updateAllBlogs = async function (req, res) {
+try{
     const blogId1 = req.params.blogId
+    if(!isValidObjectId(blogId1)){
+    return res.send("BlogId is not valid")
+    }
+    let available = await blogmodel.findById(blogId1)
+    if(!available){
+    return res.status(400).send({status: false, msg: "BlogId not found"})
+    }
     const { title, body, tags, subcategory } = req.body
     const findBlog = await blogmodel.findById(blogId1)
     const tagsData = findBlog.tags
@@ -23,7 +32,10 @@ const updateAllBlogs = async function (req, res) {
         }
     }, { new: true })
     return res.status(200).send({ msg: blogData })
-
-}
+ }
+catch(error){
+    res.status(400).send({msg:error})
+    console.log({msg: error})
+}};
 
 module.exports.updateAllBlogs = updateAllBlogs
