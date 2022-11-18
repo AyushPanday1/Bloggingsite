e6fe48c556d0867c691e5b0d9d4daaa3e666b05a
AyushPanday1 <ayush7426883261@gmail.com>
2022-11-18 10:41:42 +0530

    Done

--- src/Middleware/commonMiddleware.js
+++ src/Middleware/commonMiddleware.js
@@ -54,57 +54,6 @@ const authorizationbypath = async (req, res, next) => {
     }
 }
 
-// Authorisation with the data of query section ------------------------------------------
-
-// const authorisebyquery = async function (req, res, next) {
-   
-//     try {
-
-                                                                          
-//         let Loggedinuser =  req["x-api-key"].authorID                           // Taking the user id of decodedtoken from server------------     
-         
-//         let {category, authorID, tags, subcategory, isPublished} = req.query;  // Taking attributes in query section -----------------------
-       
-//         const object =                                                           // Making an object with not deleted data -------------------
-//         {
-//             isDeleted: false
-//         };
-
-//         if(category)                                                           // IF user is giving category then add it to object and so on-
-//         object.category = category;
-
-//         if(authorID) 
-//         object.authorID = authorID;
-
-//         if(tags)
-//         object.tags = tags;
-
-//         if(subcategory)
-//         object.subcategory = subcategory;
-
-//         if(isPublished == false) 
-//         object.isPublished = isPublished;
-
-//         // Here i am checking from databse with object queries and applying extra filter to be same author id of loggedinuser
-
-//         let matchedData = await blogModel.find(object).filter(x => x.authorID == Loggedinuser); 
-
-//         if(!matchedData)
-//         return res.status(404).send({status: false, msg: "No such data found."});
-
-//         // Checking if both ids are same or not----------------------------------------------------------------
-//         if ((matchedData != Loggedinuser)) {
-//             return res.status(400).send({ msg: "User Has No Access" })
-//         }
-
-//         req.object = object;
-
-//         next()
-//     }
-//     catch (error) {
-//         return res.status(500).send({status:false,msg: error.message})
-//     }
-// }
 
 
 // Exporting files --------------------------------------------------------
